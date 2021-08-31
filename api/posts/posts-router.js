c236fcca4683d5897a6bc30a27c2bbc3a8d4693b
Alex <omtmn@icloud.com>
2021-08-31 18:21:35 -0400

    post and put 90%, having slight troubles

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,4 +1,3 @@
-// implement your posts router here
 const express = require('express')
 const router = express.Router()
 const Posts = require('./posts-model')
@@ -18,8 +17,8 @@ router.get('/', async (req, res) => {
 router.get('/:id', (req, res) => {
     Posts.findById(req.params.id)
     .then((post) => {
-        post ? res.status(200).json(post) 
-            :
+        post ? res.status(200).json(post)
+        :
         res.status(404).json({
                 message: "The post with the specified ID does not exist"
             })
@@ -32,5 +31,47 @@ router.get('/:id', (req, res) => {
     })
 })
 
+router.post('/', (req, res) => {
+    const newPost = req.body
+    !newPost.title || !newPost.contents ?
+        res.status(400).json({
+            message: "Please provide title and contents for the post"
+        })
+    :
+        Posts.insert(newPost)
+        .then((post) => {
+            res.status(201).json(post)
+        })
+        .catch(() => {
+            res.status(500).json({
+                message: "There was an error while saving the post to the database"
+            })
+        })  
+    }  
+)
+
+router.put('/:id', async (req, res) => {
+    const postId = req.params.id
+    const body = req.body
+   try {
+       const post = await Posts.findById(postId)
+        if(!post) {
+            res.status(404).json({
+                message: "The post with the specified ID does not exist"
+            })
+        } else if (!body.title || !body.contents) {
+            res.status(400).json({
+                message: "Please provide title and contents for the post"
+            })
+        } else {
+            const newPost = await Posts.update(postId, body)
+            res.status(201).json(newPost)
+        }
+   } catch {
+       res.status(500).json({
+            message: "The post information could not be modified"
+       })
+   }
+})
 
 module.exports = router 
--- api/server.js
+++ api/server.js
@@ -1,5 +1,3 @@
-// implement your server here
-// require your posts router and connect it here
 const express = require('express')
 const postsRouter = require('./posts/posts-router')
 
@@ -10,9 +8,9 @@ server.use(express.json())
 server.use('/api/posts', postsRouter)
 
 server.get('/', (req, res) => {
-    res.send(`
-      <h2>Posts</h2>
-    `);
+    res.send(
+        `<h2>Posts</h2>`
+    );
   });
 
 module.exports = server
