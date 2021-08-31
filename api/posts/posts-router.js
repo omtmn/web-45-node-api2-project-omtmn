fa30f4dc10693b5f97b6727df041ae9eb15e1b50
Alex <omtmn@icloud.com>
2021-08-31 18:30:55 -0400

    still need to pass 3 tests

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -90,5 +90,20 @@ router.delete('/:id', (req, res) => {
         })
 })
 
+router.get('/:id/comments', (req, res) => {
+    const postId = req.params.id
+    postId ? res.status(404).json({
+        message: "The post with the specified ID does not exist"
+    }) : Posts.findCommentById(postId)
+    .then((comments) => {
+        res.status(200).json(comments)
+    })
+    .catch((error) => {
+        res.status(500).json({
+            message: "The comments information could not be retrieved"
+        })
+    })
+})
+
 
 module.exports = router 
